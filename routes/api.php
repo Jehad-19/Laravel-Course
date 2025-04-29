9d4aff4f818dc6e4abb314d4d37a43c60b4fc10e
Jehad <jeha710@gmail.com>
2025-04-29 16:20:43 +0200

    lec7/apis

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/products', function (Request $request) {
+    return Product::all();
+});
+
+Route::get('/products/{id}', function (Request $request , $id) {
+    return Product::find($id);
+});
+
